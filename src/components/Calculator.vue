b95502d3513e8fa0cc1e7f0bc70801bdd1e3a913
nellytadi <nellytadi@gmail.com>
2021-02-25 23:53:57 +0100

    Created Calculator view with CSS and Bootstrap

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,30 +1,50 @@
 <template>
   <div class="container">
-    <div class="display"></div>
-    <div class="buttons">
-      <div class="numbers">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>=</button>
-        <button>0</button>
-        <button>.</button>
+    <div class="row">
+      <div class="col-lg-4 col-md-7 col-sm-9 col-xs-11 mx-auto mt-5">
+        <div class="calculator">
+        <div class="display">
+          <div class="toCal">
+            789*45
+          </div>
+          <div class="result">
+            35,505
+          </div>
+        </div>
+        <div class="row buttons mx-0">
+          <div class="col-9 numbers">
+            <div>
+              <button>7</button>
+              <button>8</button>
+              <button>9</button>
+            </div>
+            <div>
+              <button>4</button>
+              <button>5</button>
+              <button>6</button>
+            </div>
+            <div>
+              <button>1</button>
+              <button>2</button>
+              <button>3</button>
+            </div>
+            <div>
+              <button>.</button>
+              <button>0</button>
+              <button>=</button>
+            </div>
+          </div>
+          <div class="col-3 symbols" >
+            <button>c</button>
+            <button>&#247;</button>
+            <button>&#xd7;</button>
+            <button>-</button>
+            <button>+</button>
+          </div>
+        </div>
+
+        </div>
       </div>
-     <div class="symbols">
-       <button>c</button>
-       <button>/</button>
-       <button>*</button>
-       <button>-</button>
-       <button>+</button>
-     </div>
-
-
     </div>
   </div>
 </template>
@@ -35,7 +55,4 @@ export default {
 </script>
 <style scoped>
 
-.display{
-
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import 'bootstrap'
+import './assets/bootstrap/css/bootstrap.min.css';
+import './assets/style.css';
+
 Vue.config.productionTip = false
 
 new Vue({
